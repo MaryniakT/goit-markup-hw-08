f5cfe8583b70ba17774fdb66cec3c03917dbb8df
MaryniakT <114291060+MaryniakT@users.noreply.github.com>
2022-11-28 21:38:15 +0200

    add

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,20 @@
 (() => {
-   const mobileMenu = document.querySelector(".js-menu-container");
-   const openMenuBtn = document.querySelector(".js-open-menu");
-
-   const toggleMenu = () => {
-      const isMenuOpen =
-         openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-      openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-      mobileMenu.classList.toggle("is-open");
-      openMenuBtn.classList.toggle("is-open");
-
-      const scrollLockMethod = !isMenuOpen
-         ? "disableBodyScroll"
-         : "enableBodyScroll";
-      bodyScrollLock[scrollLockMethod](document.body);
-   };
-
-   openMenuBtn.addEventListener("click", toggleMenu);
-
-   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove("is-open");
-      openMenuBtn.setAttribute("aria-expanded", false);
-      bodyScrollLock.enableBodyScroll(document.body);
-   });
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
+    menuList: document.querySelector(".mobile-menu"),
+  };
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.menuList.addEventListener("click", removeMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+  function removeMenu() {
+    refs.menu.classList.add("is-hidden");
+    refs.body.classList.remove("no-scroll");
+  }
 })();
